4044f420aa814b4960e790b6fadf22b26cabd534
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-05 12:08:20 +0100

    [ add ] input get for research

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,7 +2,11 @@
     <nav class="navbar navbar-dark h-100">
         <div class="container-fluid d-flex align-items-center">
             <a href="{{ route('home') }}" target="_blank" class="navbar-brand">Vai al sito</a>
-            <h1 class="text-success m-0">Boolfolio</h1>
+
+            <form action="{{ route('admin.projects.index') }}" method="GET">
+                <input name="toSearch" type="search" class="form-control" placeholder="Search" aria-label="Search">
+            </form>
+
             <div class="d-flex">
                 <a class="text-success text-decoration-none fs-5 me-3"
                     href="{{ route('profile.edit') }}">{{ Auth::user()->name }}</a>
