2e04437e0f3ac0efc135f796f6d2371e0aa0a443
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-05 12:02:26 +0100

    [ add ] profile name with route edit profile

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -3,10 +3,14 @@
         <div class="container-fluid d-flex align-items-center">
             <a href="{{ route('home') }}" target="_blank" class="navbar-brand">Vai al sito</a>
             <h1 class="text-success m-0">Boolfolio</h1>
-            <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
-                @csrf
-                <button class="btn btn-light" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
-            </form>
+            <div class="d-flex">
+                <a class="text-success text-decoration-none fs-5 me-3"
+                    href="{{ route('profile.edit') }}">{{ Auth::user()->name }}</a>
+                <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
+                    @csrf
+                    <button class="btn btn-light" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
+                </form>
+            </div>
         </div>
     </nav>
 </header>
